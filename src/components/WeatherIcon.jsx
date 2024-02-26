d2ea2eb7e65f26ac6ea623cd3c8c700581ca9f88
Kalle Nyman <kalle.nyman@tuni.fi>
2024-02-27 00:11:20 +0200

    redundant expression

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -7,15 +7,12 @@ import PropTypes from "prop-types"
 const WeatherIcon = ({ iconCode }) => {
 	if (!iconCode) return <></>
 	return (
-		iconCode ?
-			<img
-				src={`https://openweathermap.org/img/wn/${iconCode}@2x.png`}
-				alt=""
-				width={67}
-				height={67}
-			/>
-			:
-			<></>
+		<img
+			src={`https://openweathermap.org/img/wn/${iconCode}@2x.png`}
+			alt=""
+			width={67}
+			height={67}
+		/>
 	)
 }
 
